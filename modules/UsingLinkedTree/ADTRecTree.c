682ac9a8cfffba24ecf6d1dd6c28ea4f2266c9cb
stelios1107 <60539477+stelios1107@users.noreply.github.com>
2023-05-25 12:32:34 +0300

    Add files via upload

---
+++ modules/UsingLinkedTree/ADTRecTree.c
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////////////
+//
+// Υλοποίηση του ADT RecTree μέσω links
+//
+///////////////////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include "ADTRecTree.h"
+
+
+// Προς υλοποίηση...
+
+
+//  Ένα δέντρο είναι pointer σε αυτό το struct
+struct rec_tree {
+
+};
+
+
+// Δημιουργεί και επιστρέφει ένα νέο δέντρο, με τιμή (στη ρίζα) value και υποδέντρα left και right.
+
+RecTree rectree_create(Pointer value, RecTree left, RecTree right) {
+	return NULL;
+}
+
+// Επιστρέφει τον αριθμό στοιχείων που περιέχει το δέντρο.
+
+int rectree_size(RecTree tree) {
+	return 0;
+}
+
+// Ελευθερώνει όλη τη μνήμη που δεσμεύει το δέντρο tree.
+
+void rectree_destroy(RecTree tree) {
+
+}
+
+// Επιστρέφουν την τιμή (στη ρίζα), το αριστερό και το δεξί υποδέντρο του δέντρου tree.
+
+Pointer rectree_value(RecTree tree) {
+	return NULL;
+}
+
+RecTree rectree_left(RecTree tree) {
+	return NULL;
+}
+
+RecTree rectree_right(RecTree tree) {
+	return NULL;
+}
+
